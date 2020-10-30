edc7b1e841128ba50ab9babd07cb2534e93bb102
Rob Clarke <robclarke93@yahoo.co.uk>
2020-10-30 16:05:04 -0700

    updated some mistakes in js file

--- js/menu-select.js
+++ js/menu-select.js
@@ -6,12 +6,12 @@ select.addEventListener('change', setTattoo);
 function setTattoo {
     const choice = select.value;
 
-     if (choice == 'arm') {
-         para.textContent = 'you have selected arm'
-     } else if (choice == 'leg') {
-         para.textContent = 'you have selected leg'
-     } else if (choice == 'chest') {
-         para.textContent = 'you have selected chest' 
+     if (choice === 'arm') {
+         para.textContent = 'you have selected arm';
+     } else if (choice === 'leg') {
+         para.textContent = 'you have selected leg';
+     } else if (choice === 'chest') {
+         para.textContent = 'you have selected chest';
      } else {
          para.textContent = '';
      }
