7a2d4f2747979727db39e284252c0679faead713
Rob Clarke <robclarke93@yahoo.co.uk>
2020-11-04 19:17:22 -0800

    added pictures; working on js to improve navigation for mobile  users

---
+++ js/responsive-nav.js
@@ -0,0 +1,8 @@
+function responseNav() {
+    var x = document.getElementById("nav-bar");
+    if (x.className === "nav-bar") {
+        x.className += " responsive";
+    } else {
+        x.className = "nav-bar";
+    }
+}
--- js/sticky-header.js
+++ js/sticky-header.js
@@ -1,20 +1,20 @@
 //when the user scrolls the page, execute myFunction
 
-window.onscroll = function() {myFunction()};
+window.onscroll = function() {stickyHeader()};
 
 //get the header
 
-var header = document.getElementById("nav-bar");
+var header = document.getElementById("nav-bar-container");
 
 //get the offset position of the navbar
 var sticky = header.offsetTop;
-
 //add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position.
 
-function myFunction() {
+function stickyHeader() {
     if (window.pageYOffset > sticky) {
         header.classList.add("sticky");
     } else {
         header.classList.remove("sticky");
     }
+    
 }
